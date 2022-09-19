70befeaddc62d11178aac80e288069a7b1f02ce2
Nguyễn Phước Huy <66677307+nphuyy1901@users.noreply.github.com>
2022-09-19 09:21:31 +0700

    config yml

---
+++ src/main/java/hcmus/brightdemy/BrightdemyApplication.java
@@ -0,0 +1,13 @@
+package hcmus.brightdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrightdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hcmus/brightdemy/BrightdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmus.brightdemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrightdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
